7a6b4f2847b2cd2fe7d78e2db924e3372eb7f7b6
Abhishek00p <prajapatabhishek9326@gmail.com>
2025-05-02 02:27:17 +0530

    trying to add a color extension

---
+++ lib/core/extensions/app_color_extension.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+
+// extension AppColorExtension on Colors{
+
+//   Color  withAppOpacity(double opacity) {
+//     return Color.fromARGB(
+//       (opacity.clamp(0.0, 1.0) * 255).round(),
+//       red,
+//       green,
+//       blue,
+//     );
+//   }
+// }
