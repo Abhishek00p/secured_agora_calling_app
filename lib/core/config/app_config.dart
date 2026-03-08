a68180e29e20732fba471005d627752d28d57dcd
Abhishek Prajapat <abhishekprajapat@nimapinfotech.com>
2026-03-08 16:24:26 +0530

    UPDATE- base url to live

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  // static String _baseUrl = 'https://secured-agora-calling-app.onrender.com/'; // Fallback value
-  static String _baseUrl = "https://cd05-2409-40c0-11b7-8838-4575-312f-26f5-3142.ngrok-free.app/";
+  static String _baseUrl = 'https://secured-agora-calling-app.onrender.com/'; // Fallback value
+  // static String _baseUrl = "https://cd05-2409-40c0-11b7-8838-4575-312f-26f5-3142.ngrok-free.app/";
 
   static String get baseUrl => _baseUrl;
 }
--- lib/core/services/app_auth_service.dart
+++ lib/core/services/app_auth_service.dart
@@ -9,8 +9,8 @@ class AppAuthService {
   static AppAuthService get instance => _instance;
 
   /// Get the appropriate base URL based on platform and environment
-  // String get baseUrl => "https://secured-agora-calling-app.onrender.com/";
-  String get baseUrl => "https://cd05-2409-40c0-11b7-8838-4575-312f-26f5-3142.ngrok-free.app/";
+  String get baseUrl => "https://secured-agora-calling-app.onrender.com/";
+  // String get baseUrl => "https://cd05-2409-40c0-11b7-8838-4575-312f-26f5-3142.ngrok-free.app/";
 
   // final FirebaseFunctions _functions = FirebaseFunctions.instance; // Not used in HTTP implementation
   String? _currentToken;
